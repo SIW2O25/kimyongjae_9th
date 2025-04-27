2b618f064328ad97893df3a0dcf209c85815b844
SIW2O25 <sheltie37@naver.com>
2025-04-27 19:58:37 +0900

    과제 마지막 작성

--- ConsoleApp6/obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
+++ ConsoleApp6/obj/Debug/net8.0/ConsoleApp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2314ca9f16dfbdf9045a337538ea3242700076b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
